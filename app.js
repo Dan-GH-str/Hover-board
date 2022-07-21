89bc6cb8b052ac5ae7b41c904666bc6a20b8e164
Dan-GH-str <daniil.zhuravlev0306@mail.ru>
2022-07-21 11:02:39 +0300

    Hover-board

---
+++ app.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('#board')
+const colors = ['#58c34c', '#de9424', '#e33a32', '#3dcba4', '#3d70cc', '#6d1db8', '#e234cc', '#e1e4c2']
+const SQUARE_NUMBER = 500
+
+for (let i = 0; i < SQUARE_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+
+    square.addEventListener('mouseleave', () => {
+        removeColor(square)
+    })
+
+    board.append(square)
+}
+
+function setColor (element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = `${color}`
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor (element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const i = Math.floor(Math.random() * colors.length)    // 
+    return colors[i]
+}
